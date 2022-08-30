ca0d3a3a023ccc1f35594951ad9f0aaa786babcc
yuseonkim <yusun715@icloud.com>
2022-08-30 20:36:40 +0900

    model 생성

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const UserStorage = require("../../models/UserStorage");
 const output = {
     home : (req,res) =>{
     res.render("home/index");
@@ -12,34 +13,27 @@ const output = {
     }
 };
 
-const users = {
-    id : ["woorimIT","나개발","김팀장"],
-    psword : ["1234","1234","123456"],
-}
-
-
 const process = {
     login : (req,res) => {
          const id = req.body.id,
          psword = req.body.psword;
-
         
+        
+        const users = UserStorage.getUsers("id","psword");
 
+        const response = {};
          if(users.id.includes(id)){
             const idx = users.id.indexOf(id);
             if (users.psword[idx] === psword){
-                return res.json({
-                    success: true
-                });
-                
+                response.success = true;
+                return res.json(response);
             }
          }
 
-         return res.json({
-            success:false,
-            msg:"로그인실패",
-         })
-    }
+         response.success = false;
+         response.msg = "로그인 실패";
+         return res.json(response);
+    },
 }
 
 
