fb8ac2e87d6248cddb0f948f855ee6a12f08c027
yuseonkim <yusun715@icloud.com>
2022-08-31 14:38:43 +0900

    user 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,5 +1,7 @@
 "use strict"
 
+const User = require("./User");
+
 class UserStorage{
    static #users = {
         id : ["woorimIT","나개발","김팀장"],
@@ -18,6 +20,18 @@ class UserStorage{
         },{});
         return newUsers;
     }
+
+    static getUserInfo(id){
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        },{});
+
+        return userInfo;
+    };
 };
 
 module.exports = UserStorage;
