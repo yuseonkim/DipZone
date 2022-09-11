c75ed151cbb80682dac70df60e3d7c1be79b1818
yuseonkim <yusun715@icloud.com>
2022-09-11 21:52:14 +0900

    로그인 및 회원가입 db 연결 구현

---
+++ app/src/config/db.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+    host: "samsso.c7hnqpgwel3s.ap-northeast-2.rds.amazonaws.com",
+    user : "admin",
+    password : "616aa715",
+    database:"samsso",
+});
+
+db.connect();
+
+module.exports = db;
--- app/src/models/User.js
+++ app/src/models/User.js
@@ -10,14 +10,18 @@ class User {
 
     async login() {
         const client = this.body;
-        const { id, psword } = await UserStorage.getUserInfo(client.id);
-        if (id) {
-            if (id === client.id && psword === client.psword) {
+        try{
+        const user  = await UserStorage.getUserInfo(client.id);
+        if (user) {
+            if (user.id === client.id && user.psword === client.psword) {
                 return { success: true };
             }
             return { success: false, msg: "비밀번호가 틀렸습니다" };
         }
         return { success: false, msg: "존재하지 않는 아이디입니다" };
+    }catch(err){
+        return { success : false,  msg:err};
+    }
     }
 
     async register() {
